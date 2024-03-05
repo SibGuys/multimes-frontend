6b6a43c99b3118c61f4e3392748a8280907513a4
Lyasman <danlaysm45.work@gmail.com>
2024-03-05 13:50:05 +0600

    [+] add navar,avatars and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import Navbar from "./widgets/navbar/Navbar";
 import Spacebar from "./widgets/spacebar/Spacebar";
+import Avatar from "./featues/avatar/Avatar";
+import Space from "@/shared/ui/space/Space";
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Spacebar
-        title="Space name"
-        dialogList={[
-          {
-            messenger: "tg",
-            username: "Andrey Shelyagin",
-            isOnline: false,
-            dialogAttributes: {
-              lastMessage: "hello",
-              lastMessageTime: "12:00",
-              countOfUnreadMesaages: 1,
-            },
-          },
-        ]}
-      />
-    </>
-  );
+    return (
+         <>
+          {<div className="smth"> <Avatar name={"DY"} messengerIcon={"telegram"}></Avatar></div>}
+         </>
+     )
 };
 
 export default App;
--- src/widgets/navbar/Navbar.tsx
+++ src/widgets/navbar/Navbar.tsx
@@ -1,19 +1,26 @@
 import "./Navbar.css";
-
+import Space from "src/shared/ui/space/Space";
+import SpaceProps from "src/shared/ui/space/Space";
 import addSvg from "src/assets/svg/add.svg";
 import profileSvg from "src/assets/svg/profile.svg";
+type NavbarProps= {
+    icons: typeof SpaceProps[];
+}
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <button className="navbar__add-button">
-        <img src={addSvg} alt="Add new space button"></img>
-      </button>
-      <button className="navbar__profile">
-        <img src={profileSvg} alt="Profile button" />
-      </button>
-    </nav>
-  );
+    return (
+        <>
+            <nav className="navbar">
+                <ul className={"navbar-list"}>
+
+                    <li className="navbar-list_item"><Space chosenSpace={"add"}/></li>
+                    <li className="navbar-list_item"><Space chosenSpace={"icon-all-chat"}/></li>
+                    <li className="navbar-list_item"><Space chosenSpace={"inactive-space"} children={"name"}/></li>
+                    <li className="navbar-list_item"><Space chosenSpace={"icon-account"}></Space> </li>
+                </ul>
+            </nav>
+        </>
+    );
 };
 
 export default Navbar;
