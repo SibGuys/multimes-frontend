0e1caee71e39ca414b33ed659651d24a46256843
Mikula GerHard <mikulagerhard00@gmail.com>
2024-03-15 09:49:40 +0600

    [*] initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import Navbar from "./widgets/navbar/Navbar";
 import Spacebar from "./widgets/spacebar/Spacebar";
 import Avatar from "./featues/avatar/Avatar";
 import Space from "@/shared/ui/space/Space";
+import DialogAttributes from "./entities/dialog-attributes/DialogAttributes";
+import Dialog from "./entities/dialog/Dialog";
+
+const dialog : Dialog[] = [
+  {
+    messenger : "telegram",
+    username : "Gerhard",
+    isOnline : false,
+    dialogAttributes : {
+      lastMessage : "Hello",
+      lastMessageTime : "19:20",
+      countOfUnreadMesaages : 9
+    }
+  }
+]
 
 const App = () => {
   return (
     <>
       {
-        <div className="smth">
-          {" "}
-          <Avatar name={"DY"} messengerIcon={"telegram"}></Avatar>
-        </div>
+        <Navbar />
+        <Spacebar title="Base" dialogList={dialog}></Spacebar>
       }
     </>
   );
